2f668f3c1eb50c5dc760b61bbe905d983ad9039b
acosonic <acosonic@gmail.com>
2019-02-13 14:41:43 +0100

    updated Readme

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 Redmine::Plugin.register :mobile_issue_table do
   name 'Mobile Issue Table plugin'
-  author 'Aleksandar Pavić'
+  author 'Aleksandar Pavić & Bilel Kedidi'
   description 'Improves visibility of issue list on mobile devices'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author_url 'https://linkedin.com/in/acosonic'
 end
 
 require_dependency 'mobile_issue_table_hook_listener'
